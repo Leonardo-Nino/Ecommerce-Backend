9fe374bdd46889d4e924f53d9a4edcf5f142ecc1
Leonardo <leonardoninio@gmail.com>
2023-09-20 20:50:06 +0300

    added Router deploy

--- src/App.js
+++ src/App.js
@@ -126,7 +126,7 @@ app.use((req, res, next) => {
 
 app.use('/', express.static(__dirname + '/public'))
 app.get("/", async (req, res) => {
-  res.render("home");
+  res.redirect('/api/products');
 });
 app.use('/api/products', productsRouters)
 app.use('/api/carts', cartsRouters)
